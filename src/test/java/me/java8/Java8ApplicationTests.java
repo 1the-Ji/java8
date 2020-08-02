97b68f06d0e20099168586f959c8a1fd5cb6a8cd
1the-Ji <jish0516@gmail.com>
2020-08-02 12:28:14 +0900

    java8 setting

---
+++ src/test/java/me/java8/Java8ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.java8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
